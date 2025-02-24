e91fc5e913d1fb37f3284faa0784f1bf5a0721ee
Rubendvb <rubenvasquez556@gmail.com>
2025-02-24 17:35:12 -0300

    second parte invite page

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
-
-import logo from '../../assets/logo.svg'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
+
+import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { IconButton } from '@/components/icon-button'
+import logo from '../../assets/logo.svg'
+import gold from '../../assets/medal-gold.svg'
+import silver from '../../assets/medal-silver.svg'
+import cooper from '../../assets/medal-cooper.svg'
 
 export default function InvatePage() {
   return (
@@ -82,6 +85,50 @@ export default function InvatePage() {
 
               <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+
+        <div className="space-y-4">
+          <div className="relative bg-gray-700 rounded-xl border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1°</span> | Rubén Vásquez
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+
+            <Image src={gold} alt="" className="absolute top-0 right-6" />
+          </div>
+
+          <div className="relative bg-gray-700 rounded-xl border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">2°</span> | Rubén Vásquez
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+
+            <Image src={silver} alt="" className="absolute top-0 right-6" />
+          </div>
+
+          <div className="relative bg-gray-700 rounded-xl border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">3°</span> | Rubén Vásquez
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+
+            <Image src={cooper} alt="" className="absolute top-0 right-6" />
           </div>
         </div>
       </div>
